2fce303939bda3c58364a81e3a31d870200ef9e5
Kyrellos Rezk <kyrellos.rezk@egyptianservicecard.com>
2025-10-22 11:48:38 +0300

    fix bug in reacts count per post

--- backend-source-code/post-management-service/src/main/java/com/fawry/post_management_service/repositories/PostRepository.java
+++ backend-source-code/post-management-service/src/main/java/com/fawry/post_management_service/repositories/PostRepository.java
@@ -19,21 +19,34 @@ public interface PostRepository extends JpaRepository<PostEntity, String> {
         p.content AS content,
         p.user_id AS userId,
         p.has_media AS hasMedia,
-        COUNT(DISTINCT c.id) AS commentsCount,
-        COALESCE(SUM(CASE WHEN r.type = 'LIKE' THEN 1 ELSE 0 END), 0) AS likeCount,
-        COALESCE(SUM(CASE WHEN r.type = 'DISLIKE' THEN 1 ELSE 0 END), 0) AS disLikeCount
+        COALESCE(c.commentsCount, 0) AS commentsCount,
+        COALESCE(r.likeCount, 0) AS likeCount,
+        COALESCE(r.disLikeCount, 0) AS disLikeCount
     FROM post_management.post p
-    LEFT JOIN post_management.comment c ON p.id = c.post_id
-    LEFT JOIN post_management.react r ON p.id = r.post_id
+    LEFT JOIN (
+        SELECT 
+            post_id, 
+            COUNT(DISTINCT id) AS commentsCount
+        FROM post_management.comment
+        GROUP BY post_id
+    ) c ON p.id = c.post_id
+    LEFT JOIN (
+        SELECT 
+            post_id,
+            SUM(CASE WHEN type = 'LIKE' THEN 1 ELSE 0 END) AS likeCount,
+            SUM(CASE WHEN type = 'DISLIKE' THEN 1 ELSE 0 END) AS disLikeCount
+        FROM post_management.react
+        GROUP BY post_id
+    ) r ON p.id = r.post_id
     WHERE p.user_id = :filterUserId
-    GROUP BY p.id, p.content, p.user_id, p.has_media, p.created_at
     ORDER BY p.created_at DESC
     """,
             countQuery = """
-    SELECT COUNT(*)
-    FROM post_management.post p
-    WHERE p.user_id = :filterUserId
-    """, nativeQuery = true)
+        SELECT COUNT(*)
+        FROM post_management.post p
+        WHERE p.user_id = :filterUserId
+    """,
+            nativeQuery = true)
     Page<Object[]> findPostsByUserId(@Param("filterUserId") String filterUserId, Pageable pageable);
 
     @Query(value = """
@@ -42,22 +55,34 @@ public interface PostRepository extends JpaRepository<PostEntity, String> {
         p.content AS content,
         p.user_id AS userId,
         p.has_media AS hasMedia,
-        COUNT(DISTINCT c.id) AS commentsCount,
-        COALESCE(SUM(CASE WHEN r.type = 'LIKE' THEN 1 ELSE 0 END), 0) AS likeCount,
-        COALESCE(SUM(CASE WHEN r.type = 'DISLIKE' THEN 1 ELSE 0 END), 0) AS disLikeCount
+        COALESCE(c.commentsCount, 0) AS commentsCount,
+        COALESCE(r.likeCount, 0) AS likeCount,
+        COALESCE(r.disLikeCount, 0) AS disLikeCount
     FROM post_management.post p
-    LEFT JOIN post_management.comment c ON p.id = c.post_id
-    LEFT JOIN post_management.react r ON p.id = r.post_id
+    LEFT JOIN (
+        SELECT 
+            post_id, 
+            COUNT(DISTINCT id) AS commentsCount
+        FROM post_management.comment
+        GROUP BY post_id
+    ) c ON p.id = c.post_id
+    LEFT JOIN (
+        SELECT 
+            post_id,
+            SUM(CASE WHEN type = 'LIKE' THEN 1 ELSE 0 END) AS likeCount,
+            SUM(CASE WHEN type = 'DISLIKE' THEN 1 ELSE 0 END) AS disLikeCount
+        FROM post_management.react
+        GROUP BY post_id
+    ) r ON p.id = r.post_id
     LEFT JOIN follow_management.following f ON f.user_id = :userId
     WHERE p.user_id = :userId OR p.user_id = ANY(f.followed_user_ids)
-    GROUP BY p.id, p.content, p.user_id, p.has_media, p.created_at
     ORDER BY p.created_at DESC
     """,
             countQuery = """
-    SELECT COUNT(*)
-    FROM post_management.post p
-    JOIN follow_management.following f ON f.user_id = :userId
-    WHERE p.user_id = :userId OR p.user_id = ANY(f.followed_user_ids)
+        SELECT COUNT(*)
+        FROM post_management.post p
+        JOIN follow_management.following f ON f.user_id = :userId
+        WHERE p.user_id = :userId OR p.user_id = ANY(f.followed_user_ids)
     """,
             nativeQuery = true)
     Page<Object[]> findFeedForUser(@Param("userId") String userId, Pageable pageable);
